74417d00840cfa47b8374be5f9777c2dac91e652
Cynthia Kasambuli <cynthiakasambuli@gmail.com>
2018-04-03 09:34:56 +0300

    add first initialization test

---
+++ password.py
@@ -0,0 +1,14 @@
+class Credentials:
+    """
+    Class that generates new instances of user.
+    """
+
+    credentials= []
+
+
+
+    def __init__(self, user_name, first_name, email):
+
+        self.user_name = user_name
+        self.first_name = first_name
+        self.email = email
